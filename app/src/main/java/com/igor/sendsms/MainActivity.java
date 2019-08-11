abce4b370d8fed799f0dc32dd50cf352529e18c7
igor1965 <sreznikov_igor@hotmail.com>
2019-08-11 09:01:43 -0400

    add vibrate

--- app/src/main/java/com/igor/sendsms/MainActivity.java
+++ app/src/main/java/com/igor/sendsms/MainActivity.java
@@ -1,35 +1,86 @@
 package com.igor.sendsms;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.devspark.appmsg.AppMsg;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
     private static final int MY_PERMISSIONS_READ_CONTACTS = 1;
     private static final int PICK_CONTACT = 2;
+    private static final String SMS_SENT = "smsSent";
+    private static final String SMS_DELIVERED = "smsDelivered";
+    private static final int MAX_SMS_MESSAGE_LENGTH = 140;
     Button sendBtn;
     EditText txtphoneNo;
     EditText txtMessage;
     String phoneNo;
-    String message;
+    String mes;
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = null;
+            String action = intent.getAction();
+
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    message = "Message sent!";
+                    break;
+                case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                    message = "Error. Message not sent.";
+                    break;
+                case SmsManager.RESULT_ERROR_NO_SERVICE:
+                    message = "Error: No service.";
+                    break;
+                case SmsManager.RESULT_ERROR_NULL_PDU:
+                    message = "Error: Null PDU.";
+                    break;
+                case SmsManager.RESULT_ERROR_RADIO_OFF:
+                    message = "Error: Radio off.";
+                    break;
+            }
+            // Just replaced Toast.More information:http://johnkil.github.io/Android-AppMsg/
+
+           /* AppMsg.makeText(SendMessagesWindow.this, message,
+                    AppMsg.STYLE_CONFIRM).setLayoutGravity(Gravity.BOTTOM)
+                    .show();*/
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerReceiver(receiver, new IntentFilter(SMS_SENT));  // SMS_SENT is a constant
 
         sendBtn = (Button) findViewById(R.id.btnSendSMS);
         txtphoneNo = (EditText) findViewById(R.id.editText);
@@ -47,22 +98,57 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void shakeItBaby() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(250);
+        }
+    }
 
     protected void sendSMSMessage() {
-        //phoneNo = txtphoneNo.getText().toString();
-        message = txtMessage.getText().toString();
+        phoneNo = txtphoneNo.getText().toString();
+        mes = txtMessage.getText().toString();
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.SEND_SMS)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.SEND_SMS},
+                Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},
                         MY_PERMISSIONS_REQUEST_SEND_SMS);
-            }
         }
+        SmsManager manager = SmsManager.getDefault();
+
+        PendingIntent piSend = PendingIntent.getBroadcast(this, 0, new Intent(SMS_SENT), 0);
+        PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0, new Intent(SMS_DELIVERED), 0);
+
+       /* if(isBinary)
+        {
+            byte[] data = new byte[mes.length()];
+
+            for(int index=0; index<mes.length() && index < MAX_SMS_MESSAGE_LENGTH; ++index)
+            {
+                data[index] = (byte)mes.charAt(index);
+            }
+
+            manager.sendDataMessage(phoneNo, null, (short) SMS_PORT, data,piSend, piDelivered);
+        }
+        else
+        {*/
+            int length = mes.length();
+
+            if(length > MAX_SMS_MESSAGE_LENGTH)
+            {
+                ArrayList<String> messagelist = manager.divideMessage(mes);
+
+                manager.sendMultipartTextMessage(phoneNo, null, messagelist, null, null);
+                shakeItBaby();
+            }
+            else
+            {
+                manager.sendTextMessage(phoneNo, null, mes, piSend, piDelivered);
+                shakeItBaby();
+            }
+       // }
     }
     private void readContacts(){
         if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
@@ -104,6 +190,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
@@ -111,8 +202,9 @@ public class MainActivity extends AppCompatActivity {
             case MY_PERMISSIONS_REQUEST_SEND_SMS: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                    //SmsManager smsManager = SmsManager.getDefault();
+                    //smsManager.sendTextMessage(phoneNo, null, mes, null, null);
+                    sendSMSMessage();
                     Toast.makeText(getApplicationContext(), "SMS sent.",
                             Toast.LENGTH_LONG).show();
                 } else {
